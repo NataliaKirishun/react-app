9dc2285d1191c6e88e6289ac339b685416a3e5c7
natallia_kiryshun <Natallia_Kiryshun@epam.com>
2019-04-21 16:24:04 +0300

    Task 6/add BrowserRouter to the app

--- client/src/index.js
+++ client/src/index.js
@@ -2,25 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './container/App/App';
 import './style.less';
 import {
   store, persistor,
 } from './store/store';
 
-// if (process.env.NODE_ENV === 'production') {
-//
-// }
-//
-// if (process.env.NODE_ENV !== 'production') {
-//     console.log('development');
-// }
+const application = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 ReactDOM.render(
   (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        {application}
       </PersistGate>
     </Provider>),
   document.getElementById('root') || document.createElement('div'));
