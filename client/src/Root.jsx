8960fb98ea0d39d52dc9feb47a889a716fcbafb5
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2019-05-05 23:01:03 +0300

    Task-7/SSR-React_add_styled_components

--- client/src/Root.jsx
+++ client/src/Root.jsx
@@ -1,15 +1,22 @@
 import 'isomorphic-fetch';
 import 'babel-polyfill';
 import React from 'react';
+import styled from 'styled-components';
+import Theme from './common/Theme/Theme';
 import { hot } from 'react-hot-loader';
 
 
 const Root = () => (
   <div>
-    <h1>Server Side Renderig</h1>
+    <Title>Server Side Renderig</Title>
     <div>Hello!</div>
   </div>
 );
+
+const Title = styled.h1` 
+    width: 100%;
+    background-color: ${Theme.colors.grey};    
+`;
 
 export default hot(module)(Root);
 
