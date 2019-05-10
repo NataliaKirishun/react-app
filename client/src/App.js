6821a6cf5fc8b96cb4eb57866015d0f1c67942dd
natallia_kiryshun <Natallia_Kiryshun@epam.com>
2019-05-10 13:43:45 +0300

    Task-7/SSR-React_add code splitting using dynamic import syntax

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,40 @@ import { Route, Switch, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import Footer from './components/Footer/Footer';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
-import MainScreen from './Screens/MainScreen/MainScreen';
-import ResultsScreen from "./Screens/ResultsScreen/ResultsScreen";
-import FilmScreen from "./Screens/FilmScreen/FilmScreen";
-import NotFoundScreen from "./Screens/NotFoundScreen/NotFoundScreen";
+import Loading from './common/Components/Loading/Loading';
+import DynamicImport from "./code_splitting/DynamicImport/DynamicImport";
+
+const MainScreen = (props) => (
+  <DynamicImport load={() => import('./Screens/MainScreen/MainScreen')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />}
+  </DynamicImport>
+);
+
+const ResultsScreen = (props) => (
+  <DynamicImport load={() => import('./Screens/ResultsScreen/ResultsScreen')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />}
+  </DynamicImport>
+);
+
+const FilmScreen = (props) => (
+  <DynamicImport load={() => import('./Screens/FilmScreen/FilmScreen')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />}
+  </DynamicImport>
+);
+
+const NotFoundScreen = (props) => (
+  <DynamicImport load={() => import('./Screens/NotFoundScreen/NotFoundScreen')}>
+    {(Component) => Component === null
+      ? <Loading />
+      : <Component {...props} />}
+  </DynamicImport>
+);
 
 class App extends Component {
   render() {
@@ -15,9 +45,9 @@ class App extends Component {
         <ErrorBoundary>
           <Switch>
             <Route exact path="/" component = { MainScreen } />
-            <Route path="/search" component={ResultsScreen} />
-            <Route path="/film/:id" component={FilmScreen} />
-            <Route path="*" component={NotFoundScreen} />
+            <Route path="/search" component={ ResultsScreen } />
+            <Route path="/film/:id" component={ FilmScreen } />
+            <Route path="*" component={ NotFoundScreen } />
           </Switch>
           <Footer />
         </ErrorBoundary>
@@ -34,3 +64,4 @@ const AppWrapper = styled.div`
     display: flex;
     flex-direction: column;
 `;
+
